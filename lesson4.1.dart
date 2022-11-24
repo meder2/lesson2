80b5e90e1d1e6ca1fd185f24dc08bb24925f35e0
meder2 <118278144+meder2@users.noreply.github.com>
2022-11-24 19:19:59 +0600

    Add files via upload

---
+++ lesson4.1.dart
@@ -0,0 +1,187 @@
+void main() {
+  //task 1
+  // int a = 10;
+  // if (a == 10) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  //task 2
+//   int b = 0;
+//   if (b == 0) {
+//     print('верно');
+//   } else {
+//     print('неверно');
+//   }
+
+//   int c = 1;
+//   if (c == 0) {
+//     print('верно');
+//   } else {
+//     print('неверно');
+//   }
+
+//   int d = -3;
+//   if (d == 0) {
+//     print('верно');
+//   } else {
+//     print('неверно');
+//   }
+// }
+
+//task 3
+  // int e = 0;
+  // if (e > 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int f = 1;
+  // if (f > 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int g = -3;
+  // if (g > 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // task 4
+  // int h = 0;
+  // if (h < 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int i = 1;
+  // if (i < 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int j = -3;
+  // if (j < 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  //task 5
+  // int k = 0;
+  // if (k >= 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int l = 1;
+  // if (l >= 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int m = -3;
+  // if (m >= 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  //task 6
+  // int o = 0;
+  // if (o <= 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int p = 1;
+  // if (p <= 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int q = -3;
+  // if (q <= 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  //task 7
+  // int r = 0;
+  // if (r != 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int s = 1;
+  // if (s != 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int t = -3;
+  // if (t != 0) {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  //task 8
+  // String u = 'test';
+  // if (u == 'test') {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // String y = 'тест';
+  // if (y == 'test') {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  // int z = 3;
+  // if (z == 'test') {
+  //   print('верно');
+  // } else {
+  //   print('неверно');
+  // }
+
+  //task 9
+
+  String x = '1';
+  if (x == '1') {
+    print('верно');
+  } else {
+    print('неверно');
+  }
+
+  int s = 1;
+  if (s == '1') {
+    print('верно');
+  } else {
+    print('неверно');
+  }
+
+  int w = 3;
+  if (w == '1') {
+    print('верно');
+  } else {
+    print('неверно');
+  }
+}
