6890804810bed4b8db89fa88cd1085a1acb0906e
meder2 <118278144+meder2@users.noreply.github.com>
2022-11-28 00:39:28 +0600

    Add files via upload

---
+++ lesson5_hw.dart
@@ -0,0 +1,94 @@
+import 'dart:io';
+
+void main() {
+  // task 1
+//   int a = 10;
+//   switch (a) {
+//     case 10:
+//       print('верно');
+//       break;
+//     default:
+//       print('неверно');
+//   }
+
+  // task 2
+  // String lang = 'kg';
+  // List ru = [
+  //   'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье.'
+  // ];
+  // List en = ['monday, tuesday, wednesday, thursday, friday, saturday, sunday.'];
+  // if (lang == 'ru') {
+  //   print(ru);
+  // }
+  // if (lang == 'en') {
+  //   print(en);
+  // }
+
+  // switch (lang) {
+  //   case 'ru':
+  //     print(ru);
+  //     break;
+  //   case 'en':
+  //     print(en);
+  //     break;
+  //   default:
+  //     print('нет такого языка');
+  // }
+
+  // task 3
+  // int num = 0;
+  // switch (num) {
+  //   case 1:
+  //     print('зима');
+  //     break;
+  //   case 2:
+  //     print('весна');
+  //     break;
+  //   case 3:
+  //     print('лето');
+  //     break;
+  //   case 4:
+  //     print('осень');
+  //     break;
+
+  //   default:
+  //     print('нет такого сезона');
+  // }
+
+  // task 4
+  // stdout.write('day: ');
+  // List num = ['поесть мороженое'];
+  // List num1 = ['пойти на свиданку с девочкой с дайвиничика'];
+  // List num2 = ['похвастаться друзьям'];
+  // List num3 = ['бросить девушку'];
+  // List num4 = ['перестать общатся с друзьями'];
+  // List num5 = ['начать бизнес'];
+  // List num6 = ['купить машину маме'];
+
+  // String day = stdin.readLineSync()!;
+  // switch (day) {
+  //   case '1':
+  //     print('план на понедельник $num');
+  //     break;
+  //   case '2':
+  //     print('план на вторник $num1');
+  //     break;
+  //   case '3':
+  //     print('план на среду $num2');
+  //     break;
+  //   case '4':
+  //     print('план на четверг $num3');
+  //     break;
+  //   case '5':
+  //     print('план на пятницу $num4');
+  //     break;
+  //   case '6':
+  //     print('план на субботу $num5');
+  //     break;
+  //   case '7':
+  //     print('план на воскресенье $num6');
+  //     break;
+  //   default:
+  //     print('у вас нет плана на этот день');
+  // }
+}
